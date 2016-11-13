9ca3b594251cfac64b41bacd83c20546a7913e45
Bianca Angotti <bangotti@ualberta.ca>
2016-11-13 10:18:14 -0700

    wow. much change. such tired.

--- app/src/main/java/com/example/apate/safetoday/CallerList.java
+++ app/src/main/java/com/example/apate/safetoday/CallerList.java
@@ -3,6 +3,7 @@ package com.example.apate.safetoday;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+//import android.os.Bundle;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -20,18 +21,19 @@ import java.util.Map;
 
 public class CallerList extends BroadcastReceiver {
     // Two mapping fields for the database table
-    private MainActivity x = new MainActivity();
 
     private boolean multipleCaller(ArrayList<Date> time_stamps) {
         Date last = time_stamps.get(time_stamps.size()-1);
         long t = last.getTime();
         Date afterRemovingThirtyMins = new Date(t - (30 * 60 * 1000));
-        if (time_stamps.get(time_stamps.size()-4).after(afterRemovingThirtyMins)) return false;
+        if (time_stamps.get(time_stamps.size() - 3).after(afterRemovingThirtyMins))
+            return false;
         return true;
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        System.out.println("onReceive entered");
         TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         try {
             Class c = Class.forName(tm.getClass().getName());
@@ -39,15 +41,31 @@ public class CallerList extends BroadcastReceiver {
             m.setAccessible(true);
             ITelephony telephonyService = (ITelephony) m.invoke(tm);
             Bundle bundle = intent.getExtras();
+            //String phoneNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
             String phoneNumber = bundle.getString("incoming_number");
             Log.d("INCOMING", phoneNumber);
 
             if ((phoneNumber != null)) {
-                Map<String, ArrayList<Date>> map = x.getMap();
-                ArrayList<Date> time_stamps = map.get(phoneNumber);
-                time_stamps.add(new Date());
-                map.put(phoneNumber, time_stamps);
-                x.putMap(map);
+                System.out.println("there is a phone number: "+phoneNumber);
+                Map<String, ArrayList<Date>> map = MainActivity.getMap();
+                ArrayList<Date> time_stamps = new ArrayList<Date>();
+                if (map.containsKey(phoneNumber)) {
+                    time_stamps = map.get(phoneNumber);
+                }
+                Date stamp = new Date();
+                // create a buffer region because of double calling
+                if (time_stamps.size() > 0 && time_stamps.get(time_stamps.size()-1).before(new Date(stamp.getTime() - 2000))) {
+                    time_stamps.add(stamp);
+                    map.put(phoneNumber, time_stamps);
+                    MainActivity.putMap(map);
+                }
+                else if (time_stamps.size() == 0) {
+                    time_stamps.add(stamp);
+                    map.put(phoneNumber, time_stamps);
+                    MainActivity.putMap(map);
+                }
+
+                System.out.println(map);
 
                 //need to check all the times and numbers now....
                 // True = hang up!
--- app/src/main/java/com/example/apate/safetoday/MainActivity.java
+++ app/src/main/java/com/example/apate/safetoday/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.apate.safetoday;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -12,23 +16,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static Map<String,ArrayList<Date>> map = new HashMap<String, ArrayList<Date>>();
     Button btnEngine;
     TextView lblEngine;
     TextView lblFuel;
 
-//    Bluetooth blue = new Bluetooth();
+    // Bluetooth blue = new Bluetooth();
     // map that knows how many times people have called
-    private Map<String, ArrayList<Date>> map = new HashMap<String, ArrayList<Date>>();
+    //private Map<String, ArrayList<Date>> map = new HashMap<String, ArrayList<Date>>();
 
 //    new EchoOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
 //    new LineFeedOffObdCommand().run(socket.getInputStream(), socket.getOutputStream());
 //    new TimeoutObdCommand().run(socket.getInputStream(), socket.getOutputStream());
 //    new SelectProtocolObdCommand(ObdProtocols.AUTO).run(socket.getInputStream(), socket.getOutputStream());
-    public Map<String, ArrayList<Date>> getMap() {
+    public static Map<String, ArrayList<Date>> getMap() {
         return map;
     }
 
-    public void putMap(Map<String, ArrayList<Date>> m) {
+    public static void putMap(Map<String, ArrayList<Date>> m) {
         map = m;
     }
 
@@ -55,15 +60,58 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.READ_PHONE_STATE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.READ_PHONE_STATE},
+                        2);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 2: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-    //Bluetooth blue = new Bluetooth(this.getApplicationContext());
-    //bluetooth
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
 
+                } else {
 
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
 
-    public void ins(Map<String, ArrayList<Date>> map) {
-        this.map = map;
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
+
 }
